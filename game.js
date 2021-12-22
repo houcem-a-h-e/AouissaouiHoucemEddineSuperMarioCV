479660ce82528ccbfe62613d97f5cd0598308344
houceme-a-h-e <houcemeddine.aouissaou@esprit.tn>
2021-12-22 14:48:14 +0100

    CV English version

--- game.js
+++ game.js
@@ -9,7 +9,7 @@ kaboom({
 })
 
 // Speed identifiers
-const MOVE_SPEED = 200
+const MOVE_SPEED = 130
 const JUMP_FORCE = 360
 const BIG_JUMP_FORCE = 550
 let CURRENT_JUMP_FORCE = JUMP_FORCE
@@ -55,16 +55,16 @@ scene("game", ({ level, score }) => {
 
   const maps = [
     [
-      '                                                                                                                                                                                                                                                                    ',
-      '                                                                                                                                                                                                                                                                    ',
-      '                                                                                                                                                                                                                                                                    ',
-      '                                                                                                                                                                                                                                                                    ',
-      '                                                                                                                                                                                                                                                                    ',
-      '     %   =*=%=                                       =*=%=                                                                                                                                                                                                        +-',
-      '                            -+                                                                                                                                                                                                                                    ()',
-      '                            ()                                    -+                    -+                         -+                            -+                  +-                  +-                   +-                          +-                      ()',
-      '                            ()                                    ()                    ()                         ()                            ()                  ()                  ()                   ()                          ()                      ()',
-      '==============================   ===================================   ===================   ========================   ===========================   =================   =================    =================    ========================   =====================',
+      '                                                                                                                                                                                                                                                                                                                                                                   ',
+      '                                                                                                                                                                                                                                                                                                                                                                   ',
+      '                                                                                                                                                                                                                                                                                                                                                                   ',
+      '                                                                                                                                                                                                                                                                                                                                                                   ',
+      '                                                                                                                                                                                                                                                                                                                                                                   ',
+      '     %   =*=%=                                       =*=%=                                                                                                                                                                                                                                                                                                         ',
+      '                            -+                                                                                                                                                                                                                                                                                                                                   +-',
+      '                            ()                                    -+                    -+                         -+                            -+                  +-                  +-                   +-                          +-                      +-                  +-                +-                           +-                          ()',
+      '                            ()                                    ()                    ()                         ()                            ()                  ()                  ()                   ()                          ()                      ()                  ()                ()                           ()                          ()',
+      '==============================   ===================================   ===================   ========================   ===========================   =================   =================    =================    ========================   =====================  ==================  ================  ===========================  ==========================',
     ]
     /*[
       '£                                       £',
@@ -114,21 +114,31 @@ scene("game", ({ level, score }) => {
   ])
  
 const linebreak = '\n';
-  add([text(' Aouissaoui Houcem Eddine '  ), pos(40, 50)])
-  add([text(' Vous devez jouer le jeu \n \n et avancer afin de consulter \n \n les rubriques du CV '  ), pos(40, 250)])
-  add([text(' Education'  ), pos(500, 250)])
-  add([text(' Cycle ingenieur informatique \n \n specialite Genie Logiciels \n \n Esprit (2019 -2022)'), pos(550, 25)])
-  add([text(' Master commerce international : \n \n premiere annee \n \n Essect ( 2014 -2015)'), pos(1310,25)])
-  add([text(' Licence appliquee en economie et finance international \n \n specialite commerce international : \n \n  Essect ( 2011 -2014)'), pos(1750,25)])
-  add([text(' IT support technique : \n \n The Hive, MARS 2019 - JUIN 2021 \n \n Depannage et resolution de divers  problemes techniques\n \n de l\'application IOS LUCE \n \n operations de base de donnees via MongoDB (Robot3T),\n \n execution des requetes HTTP via POSTMAN'  ), pos(2300, 25)])
-  add([text(' Conseiller client : \n \n CONVERGYS, OCTOBRE 2018 - FÉVRIER 2019 \n \n Gestion des réclamations clients \n \n Support technique et commercial'), pos(2890,25)])
-  add([text(' Community Manager : \n \n RENAUD CONSULTING, AVRIL 2018, JUIN 2018 \n \n Publicite FB \n \n Redacteur web/reseaux sociaux'), pos(3300,50)])
-  add([text(' Designer \n \n FIVERR, 2017 \n \n concevoir des bannieres de reseaux sociaux \n \nediteur video'), pos(3700,50)])
-  add([text('Facilitateur YouthAct & Active Citizens \n \n JASMINE FOUNDATION/ BRTISH COUNCIL, \n \n OCTOBRE 2015 - DÉCEMBRE 2017 \n \n Constitution et gestion d\'un groupe de participants \n\n etude et implementation des projets regionaux '), pos(4100,25)])
-  add([text('Tresorier \n \n ATCV, JUILLET 2014 - JUILLET 2017 \n \n planification et suivi du plan budgetaire \n \n Gestion des relations financieres internes et externes'), pos(4700,25)])
-  add([text(' Experience '  ), pos(2300, 250)])
-  add([text('Societe civile'), pos(4100,250)])
-  add([text(' J\'espere que vous avez \n \n trouve ce voyage amusant :) '), pos( 4900,250)])
+add([text(' Aouissaoui Houcem Eddine '  ), pos(40, 50)])
+add([text(' You need to play the game \n \n and move forward in order \n \n to read it all :) '  ), pos(40, 250)])
+add([text('Git repertory creation \n Unit tests creation with JUnit \n Code quality testing with Sonar \nProject versions continuous upload with Nexus (Artifacts) \n Spring boot application image\n push on Docker hub\n Complete DEVOPS chain launch with\n Jenkins on a Git Push \n An email is sent on a Jenkins build failure  \n Docker containers test with Postman.'),pos(550,25)])
+add( [text(' Uesd tools:\n Sts, Sonar, Nexus,Docker, Jenkins, Postman \n\n Technologies: \n Git, Spring Boot, MySQL'),pos(680,250)])
+add([text(' University Projects'  ), pos(500, 250)])  
+add([text(' FC Kindergarten: \n \n A parent is able to add a post, \n edit it and delete it.\n A parent is able to add a comment, \n edit it and delete it. \n A parent can like or dislike a post \n A parent can like or dislike a comment\n Creation of unaccapted words dictionnary \n Parents can send and recieve textual messages \n ( made in plain JAVA)\n '), pos(1310,25)])
+add( [text('Used tools: \n Sts, VSCode, Postman \n\n Technologies: \n Html5, CSS3, Typescript,Bootstrap \n , Angular9, Java , Spring Boot, Hibernate(ORM) \n , SQL, JPQL MySQL, Git'),pos(1310,250)])
+add([text(' Personal \n project'  ), pos(1750, 250)])  
+add([text(' Super Mario CV: \n \n Create super mario game CV: \n \n Sprites are loaded from an external website \n Followed super mario scenario with observable changes \n The player is able to read my resume \n while advancing in the game\n'), pos(1750,25)])
+add( [text(' Used tools:\n VSCode \n\n Technologies: \n Html5, Javascript, Kaboom \n '),pos(1860,250)])
+
+add([text(' Sowtare Engineering degree \n \n  Esprit (2019 -2022)'), pos(2300, 25)])
+ add([text(' International trade master degree : \n \n first year \n \n Essect ( 2014 -2015)'), pos(2890,25)])
+  add([text(' International economics and finance \n bachelor degree \n \n international trade specialty : \n \n  Essect ( 2011 -2014)'), pos(3300,25)])
+add([text(' Education'  ), pos(2300, 250)])
+add([text(' Experience '  ), pos(3700, 250)])
+ add([text(' IT technical support : \n \n The Hive, MARCH 2019 - JUNE 2021 \n \n Troubleshooting and resolving various technical \n issues of our customers.\n \n Database opeartions such as search, add, update.. \n \n \n \n API testing with Postman'  ), pos(3700, 25)])
+ add([text(' Client advisor : \n \n CONVERGYS, OCTOBRE 2018 - FEBRUARY 2019 \n \n Management of customer complaints \n \n Technical and commercial support'), pos(4100,25)])
+  add([text(' Community Manager : \n \n RENAUD CONSULTING, APRIL 2018, JUNE 2018 \n \n FB ads \n \n Web/Social networks editor'), pos(4700,50)])
+ add([text(' Designer \n \n FIVERR, 2017 \n \n Social media banners design \n \Video editor'), pos(5230,50)])
+  add([text('YouthAct & Active Citizens facilitator : \n \n JASMINE FOUNDATION/ BRTISH COUNCIL, \n \n OCTOBER 2015 - DECEMBER 2017 \n \n Constitution and management of participants group \n\n Study and implementation of regional projects '), pos(6000,25)])
+  add([text('Treasurer and founding member \n \n ATCV, JULY 2014 - JULY 2017 \n \n planning and monitoring of the budget plan \n \n Management of internal and external financial relations'), pos(6600,25)])
+  //add([text(' Experience '  ), pos(2300, 250)])
+  add([text('Civil society'), pos(5600,250)])
+ add([text(' Hope you liked \n \n this trip :) '), pos( 7100,250)])
   
   function big() {
     let timer = 0
